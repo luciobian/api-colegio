cee4e81835e7cb86e4e1254fd25f6a086cf8767a
luciobian <lbiancardi91@gmail.com>
2020-03-24 22:51:30 -0300

    feat: Relaciones del modelo Subject agregadas.

--- app/Subject.php
+++ app/Subject.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subject extends Model
 {
-    //
+
+    /**
+     * Retorna el profesor de una materia.
+     *
+     * @return void
+     */
+    public function teacher()
+    {
+        return $this->hasOne('App\Teacher');
+    }
+
+
+    /**
+     * Retorna todas la notas de una materia.
+     *
+     * @return void
+     */
+    public function notes()
+    {
+        return $this->hasMany('App\Note');
+    }
+
+    
 }
