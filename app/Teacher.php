b4b6baa051c69beac01ce4a339749b3a50c59fa0
luciobian <lbiancardi91@gmail.com>
2020-03-24 22:51:54 -0300

    feat: Relaciones del modelo Teacher agregadas.

--- app/Teacher.php
+++ app/Teacher.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Teacher extends Model
 {
-    //
+
+    /**
+     * Retorna las materias a cargo de un profesor.
+     *
+     * @return void
+     */
+    public function subject()
+    {
+        return $this->belongsTo('App\Teacher');
+    }
 }
