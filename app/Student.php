fb99515803e8c9a5abd43e4c0321f61c2ebf5775
luciobian <lbiancardi91@gmail.com>
2020-03-24 22:50:54 -0300

    feat: Relaciones del model Student agregadas.

--- app/Student.php
+++ app/Student.php
@@ -5,6 +5,14 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
-{
-    //
+{   
+    /**
+     * Retorna la nota de un alumno
+     *
+     * @return void
+     */
+    public function note()
+    {
+        return $this->hasOne('App\Note');
+    }
 }
