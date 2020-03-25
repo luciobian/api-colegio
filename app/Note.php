c2fde22a12487c9c2fd27111f141343f796004a1
luciobian <lbiancardi91@gmail.com>
2020-03-24 22:50:24 -0300

    feat: Relaciones del model Note agregadas.

--- app/Note.php
+++ app/Note.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Note extends Model
 {
-    //
+    /**
+     * Retona la nota de un estudiante.
+     *
+     * @return void
+     */
+    public function student()
+    {
+        return $this->belongsTo('App\Student');
+    }
+
+    /**
+     * Retona las notas de una materia.
+     *
+     * @return void
+     */
+    public function subject()
+    {
+        return $this->belongsTo('App\Subject');
+    }
 }
